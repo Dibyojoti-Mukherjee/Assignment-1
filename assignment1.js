6c1a2ebd6249716a1bd41ab5d237594ddb575fb1
Dibyojoti Mukherjee <dibyojotimukherjee010@gmail.com>
2022-12-09 09:31:19 +0530

    2nd commit

--- assignment1.js
+++ assignment1.js
@@ -1,6 +1,6 @@
 
 function togglehide() {
-    // let header1 = document.getElementById("header1");
+    let header1 = document.getElementById("header1");
     let menu1 = document.getElementById("toggle");
     menu1.classList.toggle("hide");
     let bar= document.getElementById("menu");
